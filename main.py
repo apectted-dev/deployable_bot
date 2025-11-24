883dc6bbfe0a4f6b1b05b007b13b8e6991c0cfa4
apectted-dev <apectted@gmail.com>
2025-11-24 23:30:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
+import time
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s: %(message)s'
+)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info("START COMMAND HAS BEEN EXECUTED")
     await update.message.reply_text("Hey! Bot is running ✅")
 
 def main():
